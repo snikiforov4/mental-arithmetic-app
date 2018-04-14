bd47a3f8559d9d76b411d7beb613eb102742f92a
Serhii Nykyforov <04mailbox@gmail.com>
2018-04-14 16:38:24 +0300

    Init app with webpack

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import GameControlPanel from './components/GameControlPanel'
+
+export default {
+  name: 'App',
+  components: {
+    GameControlPanel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 2em;
+}
+</style>
---
+++ src/components/GameControlPanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameControlPanel',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
