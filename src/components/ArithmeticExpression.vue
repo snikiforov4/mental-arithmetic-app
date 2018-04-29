20d593db6122ea8de11a154261494435de6a6aa3
Serhii Nykyforov <04mailbox@gmail.com>
2018-04-29 11:58:21 +0300

    Change data format for ArithmeticExpression

--- src/components/ArithmeticExpression.vue
+++ src/components/ArithmeticExpression.vue
@@ -1,35 +1,77 @@
 <template>
-  <div class="columns breadcrumb">
-    <div class="column is-unselectable">
-      <p class="is-size-2">{{ operation }}</p>
-      <span class="is-size-6 is-uppercase has-text-grey-lighter">should equal to</span>
-      <p class="is-size-1">{{ result }}</p>
-    </div>
+  <div class="is-unselectable">
+    <p class="is-size-2">
+      <span v-for="({value, changeable}, idx) in expArgs"
+            class="fixed-arg-column"
+            :class="{changeable: changeable, 'editable': idx === editedArgIdx}"
+            :key="idx">{{ value ? value : '   '}}</span>
+    </p>
+    <p class="is-size-6 is-uppercase has-text-grey-lighter">should equal to</p>
+    <p class="is-size-1">{{ result }}</p>
   </div>
 </template>
 
 <script>
-  const OPERATORS = Object.freeze({ADDITION: '+', SUBTRACTION: '-', MULTIPLICATION: '*', DIVISION: '/'});
-
   export default {
     name: 'ArithmeticExpression',
     data() {
       return {
-        operands: [11, 21, null],
-        operators: [OPERATORS.ADDITION, OPERATORS.ADDITION],
-        result: 42
+        expArgs: [
+          {value: null, changeable: true},
+          {value: '+', changeable: false},
+          {value: 21, changeable: false},
+          {value: '+', changeable: false},
+          {value: null, changeable: true},
+        ],
+        result: 42,
+        editedArgIdx: -1,
       }
     },
-    computed: {
-      operation: function () {
-        let res = this.operands && this.operands[0] || '';
-        for (let i = 1; i < this.operands.length; i++) {
-          res += this.operators[i - 1] || this.operators[0];
-          res += this.operands[i] ? this.operands[i] : '__';
+    created() {
+      this.editedArgIdx = this.expArgs.findIndex(arg => arg.changeable);
+    },
+    methods: {
+      editNextOperand: function () {
+        const argsLen = this.expArgs.length;
+        for (let i = (this.editedArgIdx + 1) % argsLen, cnt = 0; cnt < argsLen; i = (i + 1) % argsLen, cnt++) {
+          if (this.expArgs[i].changeable) {
+            this.editedArgIdx = i;
+            return
+          }
         }
-        return res;
-      }
+      },
+      editPrevOperand: function () {
+        const argsLength = this.expArgs.length;
+        for (let i = (argsLength + this.editedArgIdx - 1) % argsLength, cnt = 0; cnt < argsLength; i = (argsLength + i - 1) % argsLength, cnt++) {
+          if (this.expArgs[i].changeable) {
+            this.editedArgIdx = i;
+            return
+          }
+        }
+      },
     }
-  }
+  };
 </script>
 
+<style scoped>
+  .fixed-arg-column {
+    white-space: pre;
+    display: inline-block;
+    min-width: 1rem;
+    min-height: 3rem;
+    padding: 0 1rem;
+    -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
+    -moz-box-sizing: border-box;    /* Firefox, other Gecko */
+    box-sizing: border-box;         /* Opera/IE 8+ */
+  }
+
+  .changeable {
+    background-color: antiquewhite;
+    border-radius: 4px;
+  }
+
+  .editable {
+    border: 2px solid coral;
+  }
+
+</style>
