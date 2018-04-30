7e56a22d003da56a0d5d015e467e05f4e11cd894
Serhii Nykyforov <04mailbox@gmail.com>
2018-04-30 10:39:13 +0300

    Add `Settings` component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app" class="container">
-    <p class="title is-1 has-text-left">Hello!</p>
+    <h2 class="title is-2 has-text-left">Hello!</h2>
     <UserProgress/>
+    <GameSettings/>
     <a class="button is-primary is-outlined is-large is-capitalized is-pulled-right">Play</a>
   </div>
 </template>
@@ -9,12 +10,14 @@
 <script>
  import TheGame from './components/TheGame'
 import UserProgress from './components/UserProgress'
+import GameSettings from './components/GameSettings'
 
 export default {
   name: 'App',
   components: {
     TheGame,
     UserProgress,
+    GameSettings,
   }
 }
 </script>
---
+++ src/components/GameSettings.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="settings is-size-5 has-text-left">
+    <h2 class="title is-2">Settings</h2>
+    <input type="range" min="1" max="15" class="slider" v-model="duration" id="duration">
+    <label for="duration">Game duration {{ duration }}</label>
+    <input type="range" min="1" max="3" class="slider" v-model="difficulty" id="difficulty">
+    <label for="difficulty">Game difficulty {{ difficulty }}</label>
+    <div>
+      <input type="checkbox" id="addition" value="addition" v-model="operations">
+      <label for="addition">addition</label>
+      <br>
+      <input type="checkbox" id="subtraction" value="subtraction" v-model="operations">
+      <label for="subtraction">subtraction</label>
+      <br>
+      <input type="checkbox" id="multiplication" value="multiplication" v-model="operations">
+      <label for="multiplication">multiplication</label>
+      <br>
+      <input type="checkbox" id="division" value="division" v-model="operations">
+      <label for="division">division</label>
+      <br>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GameSettings',
+    data() {
+      return {
+        duration: 7,
+        difficulty: 2,
+        operations: ['addition'],
+      }
+    },
+  };
+</script>
+
+<style scoped>
+  .settings {
+    margin: 2em 0;
+  }
+
+  .slider {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 10px;
+    border-radius: 5px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+  }
+
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #4CAF50;
+    cursor: pointer;
+  }
+
+  .slider::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #4CAF50;
+    cursor: pointer;
+  }
+</style>
