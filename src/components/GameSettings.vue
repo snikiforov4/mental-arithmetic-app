24372ec59d9f8df03115b575831ccb79457af474
Serhii Nykyforov <04mailbox@gmail.com>
2018-04-30 13:25:00 +0300

    Extract Slider component

--- src/components/GameSettings.vue
+++ src/components/GameSettings.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="settings content is-size-5 has-text-left">
     <h2 class="title is-2">Settings</h2>
-    <input type="range" min="1" max="15" class="slider" v-model="duration" id="duration">
-    <label for="duration">Game duration {{ duration }}</label>
-    <input type="range" min="1" max="3" class="slider" v-model="difficulty" id="difficulty">
-    <label for="difficulty">Game difficulty {{ difficulty }}</label>
+    <Slider :min="durationRange.min" :max="durationRange.max" label="Game duration"/>
+    <Slider :min="difficultyRange.min" :max="difficultyRange.max" label="Game difficulty"/>
     <CheckedList :values="operations"/>
   </div>
 </template>
 
 <script>
+  import Slider from "./Slider";
   import CheckedList from "./CheckedList";
 
   export default {
     name: 'GameSettings',
-    components: {CheckedList},
+    components: {Slider, CheckedList},
     data() {
       return {
-        duration: 7,
-        difficulty: 2,
+        durationRange: {
+          min: 1,
+          max: 15,
+        },
+        difficultyRange: {min: 1, max: 3},
         operations: ['addition', 'subtraction', 'multiplication', 'division'],
       }
     },
@@ -28,34 +30,5 @@
 <style scoped>
   .settings {
     margin: 2em 0;
-  }
-
-  .slider {
-    -webkit-appearance: none;
-    width: 100%;
-    height: 10px;
-    border-radius: 5px;
-    background: #d3d3d3;
-    outline: none;
-    opacity: 0.7;
-    -webkit-transition: .2s;
-    transition: opacity .2s;
-  }
-
-  .slider::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    width: 25px;
-    height: 25px;
-    border-radius: 50%;
-    background: #4CAF50;
-    cursor: pointer;
-  }
-
-  .slider::-moz-range-thumb {
-    width: 25px;
-    height: 25px;
-    border-radius: 50%;
-    background: #4CAF50;
-    cursor: pointer;
   }
 </style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <label>
+        <input type="range" :min="min" :max="max" class="slider" v-model="value">
+        {{ label}} {{ value }}
+      </label>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'Slider',
+    props: {
+      min: {
+        type: [String, Number],
+        required: true,
+      },
+      max: {
+        type: [String, Number],
+        required: true,
+      },
+      label: {
+        type: String,
+      },
+    },
+    data() {
+      return {
+        value: this.min,
+      }
+    },
+  }
+</script>
+<style scoped>
+
+    .slider {
+        -webkit-appearance: none;
+        width: 100%;
+        height: 10px;
+        border-radius: 5px;
+        background: #d3d3d3;
+        outline: none;
+        opacity: 0.7;
+        -webkit-transition: .2s;
+        transition: opacity .2s;
+    }
+
+    .slider::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        width: 25px;
+        height: 25px;
+        border-radius: 50%;
+        background: #4CAF50;
+        cursor: pointer;
+    }
+
+    .slider::-moz-range-thumb {
+        width: 25px;
+        height: 25px;
+        border-radius: 50%;
+        background: #4CAF50;
+        cursor: pointer;
+    }
+</style>
