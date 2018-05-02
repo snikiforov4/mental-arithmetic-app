63dd58707917b51416f32ef7e6df63b60c7cef11
Serhii Nykyforov <04mailbox@gmail.com>
2018-05-02 11:22:36 +0300

    Extract UserPage to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app" class="container">
-    <h2 class="title is-2 has-text-left">Hello!</h2>
-    <UserProgress/>
-    <GameSettings/>
-    <a class="button is-primary is-outlined is-large is-capitalized is-pulled-right">Play</a>
+    <the-user-page></the-user-page>
   </div>
 </template>
 
 <script>
- import TheGame from './components/TheGame'
-import UserProgress from './components/UserProgress'
-import GameSettings from './components/GameSettings'
+  import TheUserPage from "./components/TheUserPage";
+  import TheGame from './components/TheGame'
 
-export default {
+  export default {
   name: 'App',
   components: {
+    TheUserPage,
     TheGame,
-    UserProgress,
-    GameSettings,
   }
 }
 </script>
---
+++ src/components/TheUserPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2 class="title is-2 has-text-left">Hello!</h2>
+    <UserProgress/>
+    <GameSettings/>
+    <a class="button is-primary is-outlined is-large is-capitalized is-pulled-right">Play</a>
+</template>
+<script>
+  import GameSettings from './GameSettings'
+  import UserProgress from './UserProgress'
+
+  export default {
+    name: 'TheUserPage',
+    components: {GameSettings, UserProgress}
+  }
+</script>
